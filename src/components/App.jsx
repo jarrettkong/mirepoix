c14b791f6d6c8538b141abe661ffde81c0772076
Jarrett Kong <jarrettkong@me.com>
2019-04-23 20:58:12 -0600

    Remove API key

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,23 +5,25 @@ import Search from './Search';
 import Location from './Location';
 import './App.scss';
 
+const yelpKey = process.env.REACT_APP_YELP_API_KEY;
+
 class App extends Component {
 	state = {
 		results: []
 	};
 
 	getResults = async info => {
-		const res = await axios.get('https://api.yelp.com/v3/businesses/search', {
-			headers: {
-				Authorization:
-					'Ht7TR9L8_8gYRHWmLhwVAkhxucLGCF5xX5_TIXDqOSVBBEKZJ9rX_7VE3JrOYPdW39GW9jNtMXMxulMslkFXotM6dPhOTW9gNdNn1xIyfZUOr05f5j6CqpbfSH2fXHYx'
-			},
-			params: {
-				term: info.query,
-				location: info.location
-			}
-		});
-		console.log(res);
+		// const res = await axios.get('https://api.yelp.com/v3/businesses/search', {
+		// 	headers: {
+		// 		Authorization:
+		// 			'Ht7TR9L8_8gYRHWmLhwVAkhxucLGCF5xX5_TIXDqOSVBBEKZJ9rX_7VE3JrOYPdW39GW9jNtMXMxulMslkFXotM6dPhOTW9gNdNn1xIyfZUOr05f5j6CqpbfSH2fXHYx'
+		// 	},
+		// 	params: {
+		// 		term: info.query,
+		// 		location: info.location
+		// 	}
+		// });
+		console.log(yelpKey);
 	};
 
 	render() {
